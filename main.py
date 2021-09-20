ae1e23fbdca1d487a2ea90990439815d484f62bd
aayus <aayushpandey616@gmail.com>
2021-09-20 14:45:38 +0545

    few changes

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 """ A Gui program that lets you draw images and transform it into text so you can send it to anyone. """
 from tkinter import *
 
+
 def main():
-    """ Main window """
+
     screen = Tk()
     screen.title("Text Draw")
-    screen.geometry("800x800")
-
+    screen.geometry("800x800+0+0")
     mainloop()
+
+
 main()
